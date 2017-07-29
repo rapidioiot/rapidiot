2242feacbf1a20b8769eaa90c841d8067f8854e5
Ivana He <ivanahe@Ivanas-MacBook-Pro.local>
2017-07-29 14:54:46 -0700

    readme description, added injectTap dependency, and added List and HomePage component

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  render() {
+    return (
+      <div className="list">
+       List of nodes
+      </div>
+    );
+  }
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'; 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+
+
 import registerServiceWorker from './registerServiceWorker';
+import Component from 'react';
+import HomePage from './components/HomePage.jsx';
+import List from './components/List.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MuiThemeProvider>
+          <HomePage />
+        </MuiThemeProvider>
+        <List />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
